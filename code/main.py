8ad5d6c55ed2f7688eaf758f8d76ed7428fd3802
RobinGOUMY <r.goumy@orange.fr>
2026-04-06 02:19:37 +0200

    Adding tracker to the project

--- code/main.py
+++ code/main.py
@@ -1,8 +1,20 @@
 from screen import launcher
+from codecarbon import EmissionsTracker
+
 
 # ============================================================
 # ENTRY POINT
 # ============================================================
 
-if __name__ == "__main__":
+
+# Initialize the tracker
+tracker = EmissionsTracker()
+tracker.start()
+
+try:
+    # Code to start the launcher
     launcher()
+finally:
+    # Important: ensures the tracker stops even if the game crashes
+    emissions: float = tracker.stop()
+    print(f"CO2 emissions for this session: {emissions:.6f} kg")
